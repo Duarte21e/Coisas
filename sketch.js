bd1a1e88ff48417108ac48da696a82fd367f6eff
Duarte21e <pedro.cruz.duarte@escola.pr.gov.br>
2024-08-20 15:07:10 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+  
+}
+
+function palavraAleatoria() {
+  
+  let palavras = ["Caminhante", "Caminho", "Caminha"];
+  
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw() {
+  
+  inicializaCores();
+
+  let texto = palavraParcial(0, width);
+    
+  text(texto, 200, 200);
+  
+}
+
+function modoNoturno(horario) {
+  if (horario > 18) {
+    console.log("Você precisa ligar o modo escuro!");
+  } else {
+    console.log("Modo noturno não é necessário neste momento.");
+  }
+}
+
+modoNoturno(15);
+modoNoturno(20);
